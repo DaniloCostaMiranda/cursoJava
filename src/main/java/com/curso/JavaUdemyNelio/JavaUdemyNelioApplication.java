1d35103bac0a93084fdb90ac72201543be20052a
Danilo Miranda <danilomiranda@Danilos-MacBook.local>
2022-01-17 10:29:50 -0300

    Project created

---
+++ src/main/java/com/curso/JavaUdemyNelio/JavaUdemyNelioApplication.java
@@ -0,0 +1,13 @@
+package com.curso.JavaUdemyNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUdemyNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUdemyNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/JavaUdemyNelio/JavaUdemyNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.JavaUdemyNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUdemyNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

13208041d63ed479528871a0dbd7987ddd385b81
Danilo Miranda <danilomiranda@Danilos-MacBook.local>
2022-01-18 14:16:11 -0300

    category

--- src/main/java/com/curso/JavaUdemyNelio/config/TestConfig.java
+++ src/main/java/com/curso/JavaUdemyNelio/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.curso.JavaUdemyNelio.config;
 
+import com.curso.JavaUdemyNelio.entities.Category;
 import com.curso.JavaUdemyNelio.entities.Order;
 import com.curso.JavaUdemyNelio.entities.User;
 import com.curso.JavaUdemyNelio.entities.enums.OrderStatus;
+import com.curso.JavaUdemyNelio.repositories.CategoryRepository;
 import com.curso.JavaUdemyNelio.repositories.OrderRepository;
 import com.curso.JavaUdemyNelio.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 

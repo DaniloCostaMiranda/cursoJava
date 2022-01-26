1b998d5cb5fb48bdb9be5f8397361ac5648b1514
Danilo Miranda <danilomiranda@Danilos-MacBook.local>
2022-01-26 19:24:05 -0300

    tratamento de excessoes

--- src/main/java/com/curso/JavaUdemyNelio/services/UserService.java
+++ src/main/java/com/curso/JavaUdemyNelio/services/UserService.java
@@ -2,6 +2,7 @@ package com.curso.JavaUdemyNelio.services;
 
 import com.curso.JavaUdemyNelio.entities.User;
 import com.curso.JavaUdemyNelio.repositories.UserRepository;
+import com.curso.JavaUdemyNelio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
